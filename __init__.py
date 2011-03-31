abdfe7a7727a1db04d60fc628e7e09e9c58a48fb
Dmitry Klimanov <k-dmitry2@narod.ru>
2011-03-31 12:25:03 +0700

    second commit

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# -*- encoding: utf-8 -*-
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+
---
+++ __tryton__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+{
+    'name': 'Charts of Accounts MSFO',
+    'name_ru_RU': 'План счетов по международному стандарту отчетности',
+    'version': '1.8.0',
+    'author': 'Dmitry Klimanov',
+    'email': 'k-dmitry2@narod.ru',
+    'website': 'http://www.delfi2000.ru/',
+    'description': '''Chart of MSFO
+''',
+    'description_ru_RU': '''План счетов по международному страндарту
+''',
+    'depends': [
+        'ekd_account',
+    ],
+    'xml': [
+         'account_types.xml',
+         'account_chart.xml',
+    ],
+    'translation': [
+#        'fr_FR.csv',
+    ],
+}
